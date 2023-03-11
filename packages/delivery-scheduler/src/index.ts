7af61eda917545a10169513f59cfa9e156b47747
mrcaidev <mrcaidev@gmail.com>
2023-03-11 20:02:44 +0800

    fix(delivery-scheduler): export from barrel file

--- packages/delivery-scheduler/src/index.ts
+++ packages/delivery-scheduler/src/index.ts
@@ -1,13 +1,3 @@
-import { Graph } from "./graph";
-
-const graph = new Graph([
-  { from: "a", to: "b", weight: 4 },
-  { from: "a", to: "c", weight: 11 },
-  { from: "b", to: "a", weight: 6 },
-  { from: "b", to: "c", weight: 2 },
-  { from: "c", to: "a", weight: 3 },
-]);
-console.log(graph.getShortestPath("a", "a"));
-console.log(graph.getShortestPath("a", "c"));
-console.log(graph.getShortestPath("b", "a"));
-console.log(graph.getShortestPath("c", "b"));
+export { Good } from "./good";
+export { GoodQueue } from "./good-queue";
+export { Graph } from "./graph";
