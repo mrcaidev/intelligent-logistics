65df92c4ad3a87d026da27077b474ce6238e1743
mrcaidev <mrcaidev@gmail.com>
2023-03-11 23:24:56 +0800

    refactor(sql-parser): ast type for every statement

--- packages/sql-parser/src/parser/types.ts
+++ packages/sql-parser/src/parser/types.ts
@@ -27,33 +27,53 @@ export type Definition = {
 };
 
 /**
+ * Abstract syntax trees of SELECT statement.
+ */
+export type SelectAST = {
+  table: string;
+  fields: "*" | string[];
+  conditions: Condition[][];
+};
+
+/**
+ * Abstract syntax trees of INSERT statement.
+ */
+export type InsertAST = {
+  table: string;
+  assignments: Assignment[];
+};
+
+/**
+ * Abstract syntax trees of UPDATE statement.
+ */
+export type UpdateAST = {
+  table: string;
+  assignments: Assignment[];
+  conditions: Condition[][];
+};
+
+/**
+ * Abstract syntax trees of DELETE statement.
+ */
+export type DeleteAST = {
+  table: string;
+  conditions: Condition[][];
+};
+
+/**
+ * Abstract syntax trees of CREATE statement.
+ */
+export type CreateAST = {
+  table: string;
+  definitions: Definition[];
+};
+
+/**
  * Abstract syntax trees.
  */
 export type AST =
-  | {
-      type: "select";
-      table: string;
-      fields: "*" | string[];
-      conditions: Condition[][];
-    }
-  | {
-      type: "insert";
-      table: string;
-      assignments: Assignment[];
-    }
-  | {
-      type: "update";
-      table: string;
-      assignments: Assignment[];
-      conditions: Condition[][];
-    }
-  | {
-      type: "delete";
-      table: string;
-      conditions: Condition[][];
-    }
-  | {
-      type: "create";
-      table: string;
-      definitions: Definition[];
-    };
+  | ({ type: "select" } & SelectAST)
+  | ({ type: "insert" } & InsertAST)
+  | ({ type: "update" } & UpdateAST)
+  | ({ type: "delete" } & DeleteAST)
+  | ({ type: "create" } & CreateAST);
