ef1d3e8963d060b0e06169473dddadae60e9396e
mrcaidev <mrcaidev@gmail.com>
2023-03-24 20:19:27 +0800

    fix(backend): change PUT /goods/:id to PATCH

--- apps/backend/src/goods/router.ts
+++ apps/backend/src/goods/router.ts
@@ -5,5 +5,5 @@ export const goodsRouter: Router = Router();
 
 goodsRouter.get("/", GoodsController.findAll);
 goodsRouter.post("/", GoodsController.create);
-goodsRouter.put("/:id", GoodsController.update);
+goodsRouter.patch("/:id", GoodsController.update);
 goodsRouter.delete("/:id", GoodsController.delete);
