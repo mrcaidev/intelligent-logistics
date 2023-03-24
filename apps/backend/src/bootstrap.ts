5b468a15786242a52e7982e35171ab275ccaf19d
mrcaidev <mrcaidev@gmail.com>
2023-03-24 23:36:07 +0800

    fix(backend): better sql

--- apps/backend/src/bootstrap.ts
+++ apps/backend/src/bootstrap.ts
@@ -2,8 +2,8 @@ import { query } from "./utils/database";
 
 export async function bootstrap() {
   await query(`
-    CREATE TABLE goods (
-      id NUMERIC,
+    CREATE TABLE IF NOT EXISTS goods (
+      id TEXT,
       name TEXT,
       createdAt TEXT,
       source TEXT,
@@ -14,14 +14,14 @@ export async function bootstrap() {
   `);
 
   await query(`
-    CREATE TABLE graphs (
+    CREATE TABLE IF NOT EXISTS graphs (
       id TEXT,
       name TEXT
     )
   `);
 
   await query(`
-    CREATE TABLE edges (
+    CREATE TABLE IF NOT EXISTS edges (
       id TEXT,
       source TEXT,
       target TEXT,
--- apps/backend/src/goods/repository.ts
+++ apps/backend/src/goods/repository.ts
@@ -8,41 +8,39 @@ export class GoodsRepository {
   }
 
   public static async findById(id: string) {
-    const [good] = await query<Good>(`SELECT * FROM goods WHERE id = ${id}`);
+    const [good] = await query<Good>("SELECT * FROM goods WHERE id = $1", [id]);
     return good;
   }
 
   public static async create(good: Good) {
     const { id, name, createdAt, source, target, isVip, graphId } = good;
 
-    await query(`
-      INSERT INTO goods (id, name, createdAt, source, target, isVip, graphId)
-      VALUES (
-        ${id},
-        ${name},
-        ${createdAt},
-        ${source},
-        ${target},
-        ${isVip},
-        ${graphId}
-      )
-    `);
+    await query(
+      `
+        INSERT INTO goods (id, name, createdAt, source, target, isVip, graphId)
+        VALUES ($1, $2, $3, $4, $5, $6, $7)
+      `,
+      [id, name, createdAt, source, target, isVip, graphId]
+    );
   }
 
   public static async updateById(id: string, good: Good) {
     const { name, source, target, graphId } = good;
 
-    await query(`
-      UPDATE goods
-      SET name = ${name},
-        source = ${source},
-        target = ${target},
-        graphId = ${graphId}
-      WHERE id = ${id}
-    `);
+    await query(
+      `
+        UPDATE goods
+        SET name = $2,
+          source = $3,
+          target = $4,
+          graphId = $5
+        WHERE id = $1
+      `,
+      [id, name, source, target, graphId]
+    );
   }
 
   public static async deleteById(id: string) {
-    await query(`DELETE FROM goods WHERE id = ${id}`);
+    await query("DELETE FROM goods WHERE id = $1", [id]);
   }
 }
