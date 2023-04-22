cae04262c719e36ccb165052b4eb973e4b396e45
mrcaidev <mrcaidev@gmail.com>
2023-04-22 14:13:22 +0800

    fix(backend): foreign key constraint check

--- apps/backend/src/edge/service.ts
+++ apps/backend/src/edge/service.ts
@@ -1,3 +1,4 @@
+import { graphRepository } from "graph/repository";
 import { NotFoundError } from "utils/http-error";
 import { edgeRepository } from "./repository";
 import { CreateRequest, UpdateByIdRequest } from "./types";
@@ -14,14 +15,32 @@ async function findAll() {
 }
 
 async function create(body: CreateRequest["body"]) {
+  const { graphId } = body;
+
+  const graph = await graphRepository.findById(graphId);
+
+  if (!graph) {
+    throw new NotFoundError("物流图不存在");
+  }
+
   return edgeRepository.create(body);
 }
 
 async function updateById(id: string, body: UpdateByIdRequest["body"]) {
+  const { graphId } = body;
+
   const oldExam = await edgeRepository.findById(id);
 
   if (!oldExam) {
     throw new NotFoundError("路线不存在");
+  }
+
+  if (graphId) {
+    const graph = await graphRepository.findById(graphId);
+
+    if (!graph) {
+      throw new NotFoundError("物流图不存在");
+    }
   }
 
   await edgeRepository.updateById(id, { ...oldExam, ...body });
--- apps/backend/src/good/service.ts
+++ apps/backend/src/good/service.ts
@@ -1,3 +1,4 @@
+import { graphRepository } from "graph/repository";
 import { NotFoundError } from "utils/http-error";
 import { goodRepository } from "./repository";
 import { CreateRequest, UpdateByIdRequest } from "./types";
@@ -14,14 +15,32 @@ async function findAll() {
 }
 
 async function create(body: CreateRequest["body"]) {
+  const { graphId } = body;
+
+  const graph = await graphRepository.findById(graphId);
+
+  if (!graph) {
+    throw new NotFoundError("物流图不存在");
+  }
+
   return goodRepository.create(body);
 }
 
 async function updateById(id: string, body: UpdateByIdRequest["body"]) {
+  const { graphId } = body;
+
   const oldGood = await goodRepository.findById(id);
 
   if (!oldGood) {
     throw new NotFoundError("物品不存在");
+  }
+
+  if (graphId) {
+    const graph = await graphRepository.findById(graphId);
+
+    if (!graph) {
+      throw new NotFoundError("物流图不存在");
+    }
   }
 
   await goodRepository.updateById(id, { ...oldGood, ...body });
