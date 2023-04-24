215361ea6367320f75c3b551ab2ef1dacf549f7b
mrcaidev <mrcaidev@gmail.com>
2023-04-24 09:46:14 +0800

    refactor(frontend): style select arrow

--- apps/frontend/src/components/form/select.tsx
+++ apps/frontend/src/components/form/select.tsx
@@ -1,5 +1,6 @@
 import clsx from "clsx";
 import { ComponentProps, useId } from "react";
+import { ChevronDown } from "react-feather";
 import { Option } from "./option";
 
 type Props = ComponentProps<"select"> & {
@@ -19,21 +20,27 @@ export function Select({
   return (
     <div className="space-y-2">
       <label htmlFor={id}>{label}</label>
-      <select
-        {...rest}
-        id={id}
-        className={clsx(
-          "block min-w-0 w-full px-3 py-2 rounded border border-gray-400 outline-none focus:outline-teal-600 focus:-outline-offset-1 bg-transparent disabled:text-gray-600",
-          className
-        )}
-      >
-        {placeholder && (
-          <Option value="" disabled>
-            {placeholder}
-          </Option>
-        )}
-        {children}
-      </select>
+      <div className="relative">
+        <select
+          {...rest}
+          id={id}
+          className={clsx(
+            "peer appearance-none min-w-0 w-full px-3 py-2 rounded border border-gray-400 outline-none focus:outline-teal-600 focus:-outline-offset-1 bg-transparent disabled:text-gray-600",
+            className
+          )}
+        >
+          {placeholder && (
+            <Option value="" disabled>
+              {placeholder}
+            </Option>
+          )}
+          {children}
+        </select>
+        <ChevronDown
+          size={16}
+          className="absolute right-3 top-1/2 -translate-y-1/2 pointer-events-none"
+        />
+      </div>
     </div>
   );
 }
